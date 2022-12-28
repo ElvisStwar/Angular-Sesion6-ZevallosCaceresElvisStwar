8dfb2acf29ee4ea18f85ad452fabc1cdc5da6533
ElvisStwar <elvis.stwar@hotmail.com>
2022-12-28 12:52:25 -0500

    Creacion de modulos y componenetes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavBarComponent } from './modulos/usuarios/nav-bar/nav-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: NavBarComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
