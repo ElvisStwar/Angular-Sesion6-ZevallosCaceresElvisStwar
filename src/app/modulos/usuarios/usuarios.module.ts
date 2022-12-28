e7ebb7796b1c03982fbd58bbfe8022d30cf31eee
ElvisStwar <elvis.stwar@hotmail.com>
2022-12-28 14:32:42 -0500

    Creacion de Nosotros y Servicios

--- src/app/modulos/usuarios/usuarios.module.ts
+++ src/app/modulos/usuarios/usuarios.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ServiciosComponent } from './servicios/servicios.component';
 
 
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    NosotrosComponent,
+    ServiciosComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    NosotrosComponent,
+    ServiciosComponent
   ]
 })
 export class UsuariosModule { }
